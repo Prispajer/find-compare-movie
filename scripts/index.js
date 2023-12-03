5705932262d310c69f759b702c36a7b13538d44a
Adrian Kozieł <118553417+Prispajer@users.noreply.github.com>
2023-12-03 20:03:42 +0100

    Project optimized

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,6 @@ const autoCompleteConfig = {
   `;
   },
   onOptionSelect(movie, summarySide, columnSide) {
-    document.querySelector(".tutorial").classList.add("is-hidden");
     onMovieSelect(movie, document.querySelector(summarySide), columnSide);
   },
   inputValue(movie) {
@@ -43,50 +42,3 @@ createAutoComplete({
     onMovieSelect(movie, document.querySelector("#right-summary"), "right");
   },
 });
-
-let leftMovie;
-let rightMovie;
-
-const onMovieSelect = async (movie, summaryElement, side) => {
-  const response = await axios.get("http://www.omdbapi.com/", {
-    params: {
-      apikey: "d9835cc5",
-      i: movie.imdbID,
-    },
-  });
-  summaryElement.innerHTML = movieTemplate(response.data);
-  if (side === "left") {
-    leftMovie = response.data;
-  } else {
-    rightMovie = response.data;
-  }
-
-  if (leftMovie && rightMovie) {
-    runComparison();
-  }
-};
-
-const runComparison = () => {
-  const leftSideStats = document.querySelectorAll(
-    "#left-summary .notification"
-  );
-  const rightSideStats = document.querySelectorAll(
-    "#right-summary .notification"
-  );
-
-  leftSideStats.forEach((leftStat, index) => {
-    const rightStat = rightSideStats[index];
-    const leftSideValue = parseInt(leftStat.dataset.value);
-    const rightSideValue = parseInt(rightStat.dataset.value);
-    if (rightSideValue > leftSideValue) {
-      leftStat.classList.remove("is-primary");
-      leftStat.classList.add("is-danger");
-    } else if (leftSideValue > rightSideValue) {
-      rightStat.classList.remove("is-primary");
-      rightStat.classList.add("is-danger");
-    } else {
-      rightStat.classList.add("is-primary");
-      leftStat.classList.add("is-primary");
-    }
-  });
-};
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,3 +1,6 @@
+let leftMovie;
+let rightMovie;
+
 const debounce = (callback, delay = 1000) => {
   let timeoutId;
   return (...args) => {
@@ -8,4 +11,48 @@ const debounce = (callback, delay = 1000) => {
       callback.apply(null, args);
     }, delay);
   };
+};
+
+const onMovieSelect = async (movie, summaryElement, side) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "d9835cc5",
+      i: movie.imdbID,
+    },
+  });
+  summaryElement.innerHTML = movieTemplate(response.data);
+  if (side === "left") {
+    leftMovie = response.data;
+  } else {
+    rightMovie = response.data;
+  }
+
+  if (leftMovie && rightMovie) {
+    runComparison();
+  }
+};
+
+const runComparison = () => {
+  const leftSideStats = document.querySelectorAll(
+    "#left-summary .notification"
+  );
+  const rightSideStats = document.querySelectorAll(
+    "#right-summary .notification"
+  );
+
+  leftSideStats.forEach((leftStat, index) => {
+    const rightStat = rightSideStats[index];
+    const leftSideValue = parseInt(leftStat.dataset.value);
+    const rightSideValue = parseInt(rightStat.dataset.value);
+    if (rightSideValue > leftSideValue) {
+      leftStat.classList.remove("is-primary");
+      leftStat.classList.add("is-danger");
+    } else if (leftSideValue > rightSideValue) {
+      rightStat.classList.remove("is-primary");
+      rightStat.classList.add("is-danger");
+    } else {
+      rightStat.classList.add("is-primary");
+      leftStat.classList.add("is-primary");
+    }
+  });
 };
